61477cbf53746a26091266f3d695b616594fc5ef
Alan Liu <AlanL.Liu@yahoo.com>
2015-04-06 13:31:06 -0400

    made some stylistic changes, made buttons align, made js a bit simpler

--- app/assets/javascripts/reservation_form.js
+++ app/assets/javascripts/reservation_form.js
@@ -1,25 +1,16 @@
-// button to show/hide form 
+// hides form or reloads page depending on if form is open
 function toggle_form(){
-  if ($('#cart').is(':visible')){
+  if ($('#form').is(':visible')){
     pause_cart(); // lock the cart so users can't change it while page is reloading
-  }
-  else{
-    $('#cart').removeClass('hidden');
-  }
-} 
-
-function toggle_reservation_info(){ 
-  if ($('#reservation_info').is(':visible')){
-    $('#reservation_info').addClass('hidden');
-  }
-  else{
     location.reload();// update the info here so it's all new when we close the form
   }
-
+  else{
+    $('#form').removeClass('hidden');
+    $('#reservation_info').addClass('hidden');
+  }
 }
 
 $(document).on('click', '#edit_reservation', function () {
   toggle_form();
-  toggle_reservation_info();// hide existing information
 });
 
